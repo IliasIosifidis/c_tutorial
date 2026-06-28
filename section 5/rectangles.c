85a3c9560ef57a6d2a677235dcc8e4eb43d21d8f
IliasIosifidis <iosifidis90@gmail.com>
2026-06-28 11:17:46 +0300

    update

--- section 5/rectangles.c
+++ section 5/rectangles.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
 int main(){
-  int width = 3;
-  int height = 2;
-  int perimeter = (width + height) *2;
-  int area = width * height;
-  printf("perimeter: %d,\narea: %d\n", perimeter, area);
+  double width = 3;
+  double height = 2;
+  double perimeter = (width + height) *2;
+  double area = width * height;
+  printf("perimeter: %.2f,\narea: %.2f\n", perimeter, area);
 }
